496c8f373f1a636752eccfa543e05d36347f3255
crov <crovdoto@gmail.com>
2024-07-14 03:02:58 +0200

    we generate a whole deck now yippee

--- cards.py
+++ cards.py
@@ -1,5 +1,5 @@
 # Ace will be defined as 14
-
+import random
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
 
@@ -28,8 +28,33 @@ class Card:
 
     def __str__(self) -> str:
         return f"{self.rankName} of {self.suit}"
+
+class Deck:
+    def __init__(self, cards : list):
+        self.cards = cards
     
+    def __str__(self) -> str:
+        return f"{self.cards}"
+
+# define the standard deck of 52 playing cards
+playing_cards = []
+for suit in suits:
+    for rank in ranks:
+        playing_cards.append(Card(rank, suit))
+
+standard_deck = Deck(playing_cards)
+
+for card in standard_deck.cards:
+    print(card)
+#
+
 class Hand:
     def __init__(self, cards : list):
         self.cards = cards
         self.size = len(cards)
+
+    def findHighCard() -> Card:
+        pass
+    
+    def isPair() -> bool:
+        pass
